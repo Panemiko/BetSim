7749d07a5a96219e66449208891efb0eafc46ecc
Panem <titoluccao@gmail.com>
2023-05-25 20:39:42 -0300

    feat: add main layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { StatusBar, Text, View } from "react-native";
+
+export default function AppLayout() {
+  return (
+    <View className="bg-zinc-900 h-screen py-12">
+      <Text className="text-4xl text-center text-zinc-500 font-bold tracking-widest">TRUCOMP</Text>
+      <Slot />
+      <StatusBar barStyle={"light-content"} />
+    </View>
+  );
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { useNavigation } from "expo-router";
 
 export default function App() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-row py-4 gap-7 px-3 flex-grow-0">
+      <Pressable className="bg-yellow-500  transition-colors active:bg-yellow-600 h- p-5 rounded-lg">
+        <Text className="font-semibold text-xl">CRIAR PARTIDA</Text>
+      </Pressable>
+      <Pressable className="bg-yellow-500  transition-colors active:bg-yellow-600 h- p-5 rounded-lg">
+        <Text className="font-semibold text-xl">CRIAR PARTIDA</Text>
+      </Pressable>
     </View>
   );
 }
