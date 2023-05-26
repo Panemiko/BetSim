3ade1afaf1b674889b8ec2609622ab2175914480
Panem <titoluccao@gmail.com>
2023-05-26 19:18:23 -0300

    feat: update app name

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function AppLayout() {
   return (
     <View className="bg-zinc-900 h-screen py-12">
-      <Text className="text-4xl text-center text-zinc-500 font-bold tracking-widest">TRUCOMP</Text>
+      <Text className="text-4xl text-center text-zinc-500 font-bold tracking-widest">BetSim</Text>
       <Slot />
       <StatusBar barStyle={"light-content"} />
     </View>
